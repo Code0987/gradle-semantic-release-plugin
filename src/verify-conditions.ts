5652afc762b06e389ecfa6cbfd204ba102d26a61
code0987 <durgapalneeraj@gmail.com>
2019-07-10 01:27:42 +0530

    fix: Path bug.

--- src/verify-conditions.ts
+++ src/verify-conditions.ts
@@ -9,9 +9,10 @@ module.exports = async function verifyConditions(
   const { cwd, env, logger } = context;
   const wd = join(cwd, ((pluginConfig as any).wd || ''));
   const command = await getCommand(wd);
-  if (command !== "./gradlew") {
+  if (!command.endsWith('gradlew')) {
     throw new Error(`Gradle wrapper not found at ${wd}`);
   }
+  logger.info(`Gradle wrapper found at ${wd}. Using ${command}.`);
   const task = await getTaskToPublish(wd, env as NodeJS.ProcessEnv, logger);
   if (task === "") {
     throw new Error("No task found that can publish artifacts");
